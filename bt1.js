f129954fecfd385a02d6db3aad0e125dcd3d7987
an <anquenmatkhau@gmail.com>
2025-10-01 10:41:01 +0700

    add

--- bt1.js
+++ bt1.js
@@ -3,12 +3,4 @@ let birthYear = 2007;
 let gender = "Nam";
 const CURRENT_YEAR = 2025;
 let age = CURRENT_YEAR - birthYear;
-console.log(
-  "Tên: " +
-    studentName +
-    ", Năm sinh: " +
-    birthYear +
-    ", Giới tính: " +
-    gender +
-    ", Tuổi: " 
-);
+console.log("Tên: " + studentName + ", Năm sinh: " + birthYear + ", Giới tính: " + gender + ", Tuổi: " + age);
---
+++ bt3.js
@@ -0,0 +1,4 @@
+let firstName = "Nguyễn Phước An";
+let age = 18;
+let isMarried = false;
+console.log("Tên: " + firstName + ", Tuổi: " + age + ", Đã kết hôn: " + isMarried);
